24815f2b58a21f87f06323b48e1d47fdc9d81e5f
Infarh <shmachilin@yandex.ru>
2021-11-16 15:14:36 +0300

    Обновление до .NET6

--- UI/SolutionTemplate.MVC/Program.cs
+++ UI/SolutionTemplate.MVC/Program.cs
@@ -1,29 +1,78 @@
-using System.Threading.Tasks;
+using FluentValidation;
 
-using Microsoft.AspNetCore.Hosting;
+using MediatR;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using SolutionTemplate.DAL.Context;
+using SolutionTemplate.DAL.Sqlite;
+using SolutionTemplate.DAL.SqlServer;
 
-namespace SolutionTemplate.MVC
+var builder = WebApplication.CreateBuilder(args);
+var configuration = builder.Configuration;
+var services = builder.Services;
+
+var db_type = configuration["Database"];
+switch (db_type)
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            using var host = CreateHostBuilder(args).Build();
+    case "SqlServer":
+        services.AddSolutionTemplateDbContextSqlServer(configuration.GetConnectionString(db_type));
+        break;
 
-            using (var service_scope = host.Services.CreateScope())
-                await service_scope.ServiceProvider.GetRequiredService<IDbInitializer>().InitializeAsync();
-
-            await host.RunAsync();
-        }
-
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(host => host
-                   .UseStartup<Startup>()
-                )
-            ;
-    }
+    case "Sqlite":
+        services.AddSolutionTemplateDbContextSqlite(configuration.GetConnectionString(db_type));
+        break;
 }
+
+services.AddValidatorsFromAssembly(typeof(Program).Assembly);
+services.AddSignalR();
+services.AddMediatR(typeof(Program));
+
+services.AddApiVersioning(opt =>
+{
+    opt.DefaultApiVersion = new ApiVersion(1, 0);
+    opt.AssumeDefaultVersionWhenUnspecified = true;
+    opt.ReportApiVersions = true;
+});
+
+services.AddControllersWithViews()
+   .AddRazorRuntimeCompilation();
+
+//services.AddHealthChecks()
+//.AddCheck<BaseHealthCheck>("Base");
+
+var app = builder.Build();
+await using (var scope = app.Services.CreateAsyncScope())
+{
+    var initializer = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+    await initializer.InitializeAsync();
+}
+
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+    app.UseBrowserLink();
+}
+else
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+//app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+app.UseStatusCodePagesWithReExecute("~/Status/{0}");
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+await app.RunAsync();
